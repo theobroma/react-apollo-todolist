2081c4b56a15f40e5ba97ebd8cf52e3fbc69767a
Alexandr <theobroma222@gmail.com>
2019-07-24 00:17:30 +0300

    upd webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,68 +1,66 @@
-'use strict'
+'use strict';
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-module.exports = function makeConfig() {
-    const config = {};
-    config.entry = __dirname+'/src/app/app.jsx';
-    config.output = {
-        path:__dirname+'/dist',
-        filename: '[name].[hash].js'
-    };
-    config.resolve = {
-      extensions: ['.js', '.jsx']
+module.exports = (function makeConfig() {
+  const config = {};
+  config.entry = __dirname + '/src/app/app.jsx';
+  config.output = {
+    path: `${__dirname}/dist`,
+    filename: '[name].[hash].js',
+  };
+  config.resolve = {
+    extensions: ['.js', '.jsx'],
   };
 
-    config.devtool = 'eval-source-map';
+  config.devtool = 'eval-source-map';
 
-    config.module = {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use:{
-                    'loader':'babel-loader',
-                    options: {
-                        'presets': ['env','react','stage-0']                    }
-                },
-                exclude: /node_modules/
-            },
-            {
-              test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-              use: 'file-loader'
-            },
-            {
-                test:/\.(sass|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback:'style-loader',
-                    use: [
-                        { loader: 'css-loader' },
-                        { loader: 'sass-loader' },
-                        // { loader: 'postcss-loader' }
-                    ]
-                })
-            }
-        ]
-    }
+  config.module = {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react', 'stage-0'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  };
 
-    config.plugins = [
-        new HtmlWebpackPlugin({
-            template: __dirname +'/src/public/index.html',
-            inject:'body'
-        }),
-        new CopyWebpackPlugin([{
-            from:__dirname+'/src/public'
-        }]),
-        new ExtractTextPlugin({filename: 'css/[name].css'})
-    ]
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/public/index.html',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: __dirname + '/src/public',
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ];
 
-    config.devServer = {
-        contentBase: './src/public',
-        open: true,
-        overlay:true,
-        stats: 'minimal'
-    }
-    return config;
-}();
+  config.devServer = {
+    contentBase: `${__dirname}/dist`,
+    open: true,
+    overlay: true,
+    stats: 'minimal',
+  };
+  console.log(config.devServer);
+  return config;
+})();
